d06f8929ced867d0aef938675454fbadd6054aa6
JnsCas <jonas.casteglione@despegar.com>
2020-05-16 23:38:13 -0300

    port change. Steps list added. Headers added

--- src/main/index.js
+++ src/main/index.js
@@ -3,16 +3,22 @@ const morgan = require('morgan');
 const routes = require('./routes/index')
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 //middlewares
 //http request logger
 app.use(morgan('dev'));
 //json support
 app.use(express.json())
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
+})
 //Initialize the route handling
 app.use(routes)
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log(`Server running on port ${port}`);
 })
