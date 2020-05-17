cc9b5d5808123f3b8004a03430472fc18dea3594
JnsCas <jonas.casteglione@despegar.com>
2020-05-17 15:13:39 -0300

    new recipe added

--- src/test/services/recipes.test.js
+++ src/test/services/recipes.test.js
@@ -8,7 +8,7 @@ const {
 describe('Recipes Routes', () => {
   it('should return all recipes', () => {
     let result = getAllRecipes();
-    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result.length, 3);
   });
   it('should return Milanesa recipe', () => {
     const result = getRecipesByNameOrIngredient("milanesa")
@@ -17,8 +17,7 @@ describe('Recipes Routes', () => {
   });
   it('should return \'Arroz con pollo\' recipe by  \'Tomate\' ingredient ', () => {
     const result = getRecipesByNameOrIngredient("Tomate")
-    assert.strictEqual(result.length, 1);
-    assert.strictEqual(result[0].name, "Arroz con pollo");
+    assert.strictEqual(result.length, 2);
   });
   it('should return \'Arroz con pollo\' recipe by  \'Cebolla\' ingredient ', () => {
     const result = getRecipesByNameOrIngredient("Cebolla")
